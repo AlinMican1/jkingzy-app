951fded816feee34022771b8a9aef2d61a84765f
AlinMican <AlinMican2001@gmail.com>
2023-07-27 16:57:49 +0100

    Works on parallax effect on gallery page and home page.

--- src/components/UI/molecule/navBar.jsx
+++ src/components/UI/molecule/navBar.jsx
@@ -9,72 +9,84 @@ import { useEffect } from 'react'
 
 const NavBar = () => {
   useEffect(() => {
-    // Scroll event handling
-    const body = document.body;
-    let lastScroll = 0;
+    // const body = document.body;
+    // let lastScroll = 0;
 
+    // const handleScroll = () => {
+    //   const currentScroll = window.scrollY;
+
+    //   if (currentScroll <= 0) {
+    //     body.classList.remove("scroll-up");
+    //   } else if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    //     body.classList.remove("scroll-up");
+    //     body.classList.add("scroll-down");
+    //   } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    //     body.classList.remove("scroll-down");
+    //     body.classList.add("scroll-up");
+    //   }
+
+    //   lastScroll = currentScroll;
+    // };
+
+    // window.addEventListener("scroll", handleScroll);
+
+    
+    // const buttons = document.querySelectorAll(".default");
+
+    // const handleClick = (event) => {
+    //   buttons.forEach((btn) => btn.classList.remove("active"));
+    //   event.target.classList.add("active");
+    // };
+
+    // buttons.forEach((button) => {
+    //   button.addEventListener("click", handleClick);
+    // });
+    const headerElement = document.querySelector('header');
+    
     const handleScroll = () => {
-      const currentScroll = window.scrollY;
-
-      if (currentScroll <= 0) {
-        body.classList.remove("scroll-up");
-      } else if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-up");
-        body.classList.add("scroll-down");
-      } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-down");
-        body.classList.add("scroll-up");
+      const isScrollDown = window.scrollY > 50;
+      if (isScrollDown) {
+        headerElement.classList.add('active-navbar');
+      } 
+      else if(isScrollDown < 50){
+        headerElement.classList.remove('active-navbar');
       }
-
-      lastScroll = currentScroll;
     };
 
-    window.addEventListener("scroll", handleScroll);
-
-    // Button click event handling
-    const buttons = document.querySelectorAll(".default");
-
-    const handleClick = (event) => {
-      buttons.forEach((btn) => btn.classList.remove("active"));
-      event.target.classList.add("active");
-    };
-
-    buttons.forEach((button) => {
-      button.addEventListener("click", handleClick);
-    });
-
-    // Clean up the event listeners when the component unmounts
+    //window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-      buttons.forEach((button) => {
-        button.removeEventListener("click", handleClick);
-      });
+      window.removeEventListener('scroll', handleScroll);
+      //window.removeEventListener("scroll", handleScroll);
+      // buttons.forEach((button) => {
+      //   button.removeEventListener("click", handleClick);
+      // });
     };
   }, []); 
+
+  
+      // useEffect(() => {
+      //   const headerElement = document.querySelector('header');
+    
+      //   const handleScroll = () => {
+      //     const isScrollDown = window.scrollY > 50;
+      //     if (isScrollDown) {
+      //       headerElement.classList.add('active-navbar');
+      //     } 
+      //     else if(isScrollDown < 50){
+      //       headerElement.classList.remove('active-navbar');
+      //     }
+      //   };
+    
+      //   window.addEventListener('scroll', handleScroll);
+    
+      //   return () => {
+      //     window.removeEventListener('scroll', handleScroll);
+      //   };
+      // }, []);
     
     return (
-    
-    // <div className='nav-container'>
-    //     <div className='nav-container2'></div>
-    //     {/* <div className='line'></div> */}
-    //     <div className='button-wrapper'>
-    //         <Link href='/about'>
-    //         <Button btnText={'About'} btnVariant={'default'} btnLink={'/About'} />
-    //         </Link> 
-    //         <Link href='/gallery'>
-    //         <Button btnText={'Gallery'} btnVariant={'default'}/>
-    //         </Link>
-    //         <Link href='/'>
-    //         <Logo />
-    //         </Link>
-    //         <Link href='/myPc'>
-    //         <Button btnText={'Specs'} btnVariant={'default'}/>
-    //         </Link>
-    //         <Button btnText={'Contact'} btnVariant={'default'}/>
-    //     </div>
-    // </div>
-    
-    <header>
+    <header >
         <div className='inner-flex'>
            <div><Logo /></div>
         <nav>
--- src/components/UI/organism/videoBg.jsx
+++ src/components/UI/organism/videoBg.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import './videoBg.css'
 import trailer from '../../../assets/trailer.mp4'
@@ -5,20 +6,59 @@ import Button from '../atom/button'
 import Reveal from '@/components/animations/reveal'
 import ContentBox from '../molecule/contentBox'
 import YoutubeVids from '../molecule/youtubeVids'
+import { useRef , useEffect } from 'react'
+import Plx from 'react-plx'
 
 const VideoBg = () => {
+
   return (
     <div className='video-container'>
-      
-            <video src={trailer} autoPlay loop muted type="video/mp4"/>
+              
+            <video  src={trailer} autoPlay loop muted type="video/mp4"/>
+            
             <div className='video-overlay' />
             
+            <Plx
+            parallaxData={[{
+              start: "self",
+              startOffset: 0,
+              duration: "80vh",
+              easing:"ease-in",
+              properties: [
+                {
+                    startValue:0,
+                    endValue:-10,
+                    unit:"vh",
+                    property:"translateY"
+                }
+              ]
+            }
+          ]}
+            >
+            <div className='video-container-fade'></div>
+            </Plx>
+            <Plx
+            parallaxData={[{
+              start: 0,
+              duration: "100vh",
+              easing:"ease-in",
+              properties: [
+                {
+                    startValue:-50,
+                    endValue:-90,
+                    unit:"vh",
+                    property:"translateY"
+                }
+              ]
+            }
+          ]}
+            >
             <div className='video-title'>
-            <h1>Welcome to <br/> KingzAce's hub </h1>
+            <h1 className='video-title-text' >KingzAce <br/><p > Avitation </p> </h1>
             
-            
-             
             </div> 
+            </Plx>
+          
             
             
 
