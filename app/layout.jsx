91e9d7d7a83db3a49047f1fe7e65c9f1710cd643
AlinMican <AlinMican2001@gmail.com>
2023-08-11 00:49:13 +0100

    minor changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,11 +4,10 @@ import NavBar from '../src/components/UI/molecule/navBar'
 import {useEffect, useRef, Suspense  } from 'react';
 import Loading from './loading';
 import Footer from '../src/components/UI/atom/footer'
-import { Poppins } from 'next/font/google'
+import { Poppins } from '@next/font/google'
 
 const poppins = Poppins({
-  weight: ['100' , '200', '300' , '400', '500' , '600' ,'700' , '800', '900'],
-  style: ['normal', 'italic'],
+  
   subsets: ['latin'],
   display: 'swap',
 })
