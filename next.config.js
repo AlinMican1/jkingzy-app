9464a539d33dc51e11ffd9dba53ae08325a886b2
AlinMican <AlinMican2001@gmail.com>
2023-08-11 15:02:58 +0100

    text-bug#5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    optimizeFonts: false,
+}
 
 module.exports = nextConfig
+
 const withVideos = require('next-videos')
 
 module.exports = withVideos()
